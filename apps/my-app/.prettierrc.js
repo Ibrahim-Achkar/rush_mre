cb63c9bd8657c50300037d385e760ef2729cf11c
Ibrahim Achkar <ibaemail@gmail.com>
2023-07-21 14:46:33 +1000

    initial commit

---
+++ apps/my-app/.prettierrc.js
@@ -0,0 +1,9 @@
+// Documentation for this file: https://prettier.io/docs/en/configuration.html
+module.exports = {
+  printWidth: 110,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
---
+++ apps/my-app/tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{html,js,ts,jsx,tsx}'],
+  media: false,
+  theme: {
+    fontFamily: {
+      sans: ['Roboto', '-apple-system', 'system-ui', 'Helvetica', 'sans-serif'],
+      icon: ['dashpivot-icons'],
+    },
+  },
+};
