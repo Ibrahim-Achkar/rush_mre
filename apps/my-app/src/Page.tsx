3debd263c426c24d1b079ef6aac220d9e13b5518
Ibrahim Achkar <ibaemail@gmail.com>
2023-07-21 15:19:02 +1000

    second commit

--- apps/my-app/src/Page.tsx
+++ apps/my-app/src/Page.tsx
@@ -1,5 +1,5 @@
 export const NotFound = () => (
-  <div className="mt-2 flex flex-col space-y-2">
-    <h2>This is page</h2>
+  <div className=" mt-2 flex flex-col space-y-2">
+    <h2>Thiss is page</h2>
   </div>
 );
